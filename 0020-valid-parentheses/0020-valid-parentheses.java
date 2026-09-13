0eb876600d2ee8d4e534c31c566e0ef1d371a21b
Sivasree Marayil <sivasreeonline@gmail.com>
2026-09-13 21:12:15 +0530

    Time: 3 ms (85.9%), Space: 43.3 MB (57.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,37 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution 
+{
+    public boolean isValid(String s) 
+    {
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char ch : s.toCharArray()) 
+        {
+
+            if (ch == '(') 
+            {
+                stack.push(')');
+            } 
+            else if (ch == '[') 
+            {
+                stack.push(']');
+            } 
+            else if (ch == '{') 
+            {
+                stack.push('}');
+            } 
+            else 
+            {
+                if (stack.isEmpty() || stack.pop() != ch) 
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
